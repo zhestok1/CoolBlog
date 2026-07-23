efb53316bf190df0bcb6d2590eded1131e6c509c
zhestok1 <rp3truhin@yandex.ru>
2026-07-23 10:35:33 +0400

    Добавил post_detail и модель Комментария

--- blog_project/blog/urls.py
+++ blog_project/blog/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.post_list, name='home'),
+    path('<int:id>/', views.post_detail, name='post_detail'),
 ]
--- blog_project/blog/views.py
+++ blog_project/blog/views.py
@@ -1,8 +1,17 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Post
+from django.db.models import Count
 
 def post_list(request):
-    posts = Post.objects.all()
+    posts = Post.objects.annotate(comments_count=Count('comments'))
     return render(request, 'blog/post_list.html', {'posts':posts})
+
+def post_detail(request, id):
+    post = get_object_or_404(Post, id=id)
+    return render(request, 'blog/post_detail.html', {'post':post})
+
+
+    
+    
 
 
